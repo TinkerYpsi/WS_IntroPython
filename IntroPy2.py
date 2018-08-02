734469a99e81b4b474493b16de1216254b482ff9
Patrick Neggie <32044099+patrick9958@users.noreply.github.com>
2018-08-02 15:40:04 -0400

    added spaceships

--- IntroPy2.py
+++ IntroPy2.py
@@ -28,3 +28,59 @@ print("Charisma: " + str(charisma))
 print("Strength: " + str(strength))
 print("Dexterity: " + str(dexterity))
 print("Intelligence: " + str(intelligence))
+
+move = input("Would you like to attack, charm, manipulate, or run away?")
+print("Heads or tails?")
+guess = input("H or T:")
+if guess is "H":
+    guess = int(1)
+else:
+    guess = int(0)
+chance = random.randomint(0, 1)
+if guess == 1:
+    if chance == 1:
+        print("Heads! You guessed correctly!")
+    else:
+        print("Oof! It was tails.")
+else if guess == 0:
+    if chance == 0:
+        print("Tails! You guessed correctly!")
+    else:
+        print("Oof! It was heads.")
+
+randomNum = random.randomint(1, 10)
+
+if move == "run away":
+    if dexterity > oppDexterity:
+        if guess == chance:
+            if randomNum > 3:
+                print("With your clearly superior dexterity, you managed to outmaneuver your opponent")
+                print("+2 dexterity")
+                print("+2 health")
+                dexterity += 2
+                health += 2
+            else:
+                print("Even with your greater dexterity and luck, you managed to trip and fall :(")
+                print("-2 health")
+                health -= 2
+        else:
+            if randomNum > 7:
+                print("You defied the odds! Even with your terrible luck, you outran your opponent")
+                print("+1 dexterity")
+                dexterity += 1
+            else:
+                print("Today is not your lucky day. While running away, you slipped and hit your head on the table")
+                print("-15 health")
+                health -= 15
+    else:
+        if guess == chance:
+            if randomNum > 7:
+                print("Today is clearly your lucky day. Even though your opponent has the legs of a cheetah, you managed to outmaneuver him. Well done!")
+                print("+2 dexterity")
+                print("+5 health")
+                dexterity += 2
+                health += 5
+            else:
+                print("It almost looked like you were getting away, but then you tripped on a cord and now you have a black eye :(")
+                print("-5 health")
+                health -= 5
--- IntroPyCurrent.py
+++ IntroPyCurrent.py
@@ -4,27 +4,62 @@ print("Welcome to my game!\n")
 userName = input("What is your name? ")
 print("\nWelcome, " + userName + "!")
 
-print("\nLet's select some character stats for you...")
+print("\nLet's select a spaceship for you...")
 randomSeed = input("Pick a number from 1 to 1000: ")
 randomSeed = int(randomSeed)
 random.seed(randomSeed)
 
 # Starting ranges for character traits
 minInitialHealth = 10
-maxInitialHealth = 30
+maxInitialHealth = 100
 minInitialTrait = 1
 maxInitialTrait = 10
 
 # Select random character traits
 health = random.randint(minInitialHealth, maxInitialHealth)
-charisma = random.randint(minInitialTrait, maxInitialTrait)
-strength = random.randint(minInitialTrait, maxInitialTrait)
-dexterity = random.randint(minInitialTrait, maxInitialTrait)
-intelligence = random.randint(minInitialTrait, maxInitialTrait)
+fuel = random.randint(minInitialTrait, maxInitialTrait)
+dodge = random.randint(minInitialTrait, maxInitialTrait)
+firepower = random.randint(minInitialTrait, maxInitialTrait)
+luck = random.randint(minInitialTrait, maxInitialTrait)
 
-print("Here's how you turned out:")
+print("Here's what you got:")
 print("Health: " + str(health))
-print("Charisma: " + str(charisma))
-print("Strength: " + str(strength))
-print("Dexterity: " + str(dexterity))
-print("Intelligence: " + str(intelligence))
+print("Fuel: " + str(fuel))
+print("Dodge: " + str(dodge))
+print("Firepower: " + str(firepower))
+print("Luck: " + str(luck))
+
+# Select random character traits
+enemyHealth = random.randint(minInitialHealth, maxInitialHealth)
+enemyFuel = random.randint(minInitialTrait, maxInitialTrait)
+enemyDodge = random.randint(minInitialTrait, maxInitialTrait)
+enemyFirepower = random.randint(minInitialTrait, maxInitialTrait)
+enemyLuck = random.randint(minInitialTrait, maxInitialTrait)
+
+print("The galaxy has been under attack and we need you to wipe out some space pirates in Quadrant 4 Section 15")
+accept = input("Do you accept the challenge? [Y] or [N]")
+
+if accept == "Y" or accept == "y":
+    print("Take off!")
+    print("Look, an angry pirate is shooting at us!")
+    scan = input("Would you like to scan his ship? [Y] or [N]")
+    if scan == "Y" or scan == "y":
+        print("Scanning...")
+        print("Here's the info for his ship:")
+        print("Health: " + str(enemyHealth))
+        print("Fuel: " + str(enemyFuel))
+        print("Dodge: " + str(enemyDodge))
+        print("Firepower: " + str(enemyFirepower))
+        print("Luck: " + str(enemyLuck))
+        print("Would you like to flee, dodge, or attack?")
+        option = input("Press [F] for flee, [D] for dodge, or [A] for attack")
+    else:
+        print("Would you like to flee, dodge, or attack?")
+        option = input("Press [F] for flee, [D] for dodge, or [A] for attack")
+
+
+
+
+
+
+
