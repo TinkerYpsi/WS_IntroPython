7bbb0ae98a9f2837e60f487735f234be0198ba11
mploof <michael.ploof@gmail.com>
2018-08-06 12:25:34 -0400

    added VarsListsConditional

---
+++ VarsListsConditionals.py
@@ -0,0 +1,8 @@
+# Player stats
+name = "Garf"                                       # Name (string)
+age = 135                                           # Age in years (int)
+height = 2.3                                        # Height in meters (float)
+
+# Inventory items
+backpack = {"cat", "baseball bat", "star dust"}     # This is a set because it is unordered and contains no duplicates
+utility_belt = ["knife", "water bottle", "knife"]   # This is a list because it has an order and may contain duplicates
