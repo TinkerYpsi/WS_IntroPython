17e75b06dd363f000dc63ea5c047b0b60d90e66b
Patrick Neggie <patar9958@users.noreply.github.com>
2018-08-07 03:33:28 -0400

    added csv writing capabilities, adding habits, moved habit logging to class func
    still needs debugging

--- habitica.py
+++ habitica.py
@@ -1,3 +1,7 @@
+import os.path
+import sys
+
+
 class Player:
     def __init__(self, name, health=100, xp=0, habits=[]):
         self.name = name
@@ -9,18 +13,25 @@ class Player:
         self.alive = True
 
     # frequency is number of times per week habit should be done
-    def add_habit(self, habit):
-        self.habits.append(habit)
+    def add_habit(self, habit_name=""):
+        if habit_name == "":
+            habit_name = input("New habit description: ")
+        self.habits.append(habit_name)
         self.successes.append(0)
         self.failures.append(0)
 
     def check_habit(self, habit, status):
+        index = get_index(self, habit)
+        if index == -1:
+            print("ERROR: Habit not found!")
+            return
+
         health_bonus = 5
         health_penalty = 10
         xp_bonus = 5
         xp_penalty = 10
         if status == True:
-            self.successes[get_index(self, habit)] += 1
+            self.successes[index] += 1
             self.xp += xp_bonus
             if self.health + health_bonus >= 100:
                 self.health = 100
@@ -29,7 +40,7 @@ class Player:
             else:
                 self.health += health_bonus
         else:
-            self.failures[get_index(self, habit)] += 1
+            self.failures[index] += 1
             self.xp -= xp_penalty
             if self.health - health_penalty <= 0:
                 self.alive = False
@@ -54,6 +65,53 @@ class Player:
         success_ratio = total_success / (total_success + total_failure)
         print("CONSISTENCY: " + str(success_ratio) + "%\n")
 
+    def get_data(self, log_exists=False):
+        num_habits = len(self.habits)
+
+        # adds a header row if log doesn't already exists
+        if not log_exists:
+            header = "Habit,Successes,Failures,Consistency"
+            data = [None] * num_habits + 1
+            data[0] = header
+        else:
+            data = [None] * num_habits
+
+
+        # iterate through all habits
+        for habit in range(0, num_habits):
+            index = get_index(self, habit)
+            row = ""
+            row += habit
+            row += ","
+            num_successes = self.successes[index]
+            row += str(num_successes)
+            row += ","
+            num_failures = self.failures[index]
+            row += str(num_failures)
+            success_ratio = num_successes / (num_successes + num_failures)
+            row += success_ratio
+            data[index] = row
+
+        return data
+
+    def log_habits(self):
+        for habit in self.habits:
+            answered = False
+            while not answered:
+                habit_status = input("Did you do " + habit + " today?  [Y] or [N]   ")
+                if habit_status == 'Y' or habit_status == 'y':
+                    self.check_habit(habit, True)
+                    answered = True
+                if habit_status == 'N' or habit_status == 'n':
+                    self.check_habit(habit, False)
+                    answered = True
+                else:
+                    print("Be sure to type either [Y] or [N] as your answer.")
+
+
+
+
+
 
 def get_index(player, habit):
     location = 0
@@ -68,22 +126,48 @@ def get_index(player, habit):
 
 
 # create new player with 0 XP and no habits
-player = Player("player")
-player.add_habit("Bike 10 miles")
-
-print("Hi there", player.name)
-
-for habit in player.habits:
-    answered = False
-    while not answered:
-        habit_status = input("Did you do " + habit + " today?  [Y] or [N]   ")
-        if habit_status == 'Y' or habit_status == 'y':
-            player.check_habit(habit, True)
-            answered = True
-        if habit_status == 'N' or habit_status == 'n':
-            player.check_habit(habit, False)
-            answered = True
+player = Player("Patrick")
+looping = True
+while looping:
+    print("Hi there", player.name)
+    decision = input("Please press [1] to log habits, [2] to add a habit, [3] to see your stats, or [4] to quit: ")
+    log = 1
+    add = 2
+    stats = 3
+    quit = 4
+    while decision != log or decision != add or decision != quit:
+        if decision == log:
+            player.log_habits()
+        if decision == add:
+            player.add_habit()
+        if decision == stats:
+            player.print_basic_stats()
+        if decision == quit:
+            sys.exit(0)
         else:
-            print("Be sure to type either [Y] or [N] as your answer.")
+            print("Incorrect usage!")
+            decision = input("Please press [1] to log habits, [2] to add a habit, [3] to see your stats, or [4] to quit: ")
 
-player.print_basic_stats()
+
+    filename = 'habit_log.csv'
+    path = "C:/User/Documents/WS_IntroPython/habit_log.csv"
+    path += str(filename)
+    log_exists = os.path.isfile(filename)
+    if log_exists:
+        # data is a List of strings to be written to file
+        data = player.get_data(log_exists)
+
+    # w+ means that we will write to the csv file
+    # and create one if 'habit_log.csv' doesn't already exist
+    with open(filename,'w+') as file:
+        for line in data:
+            file.write(line)
+            file.write('\n')
+    file.close()
+
+    print("Nice :)")
+    print("Would you like to do something else or leave?")
+    decision = input("Please press [1] to leave or any button to stay")
+    leave = 1
+    if decision == leave:
+        looping = False
