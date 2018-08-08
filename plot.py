ad3724e3ea1498be733bc3ab2f57c744f3a95c5f
Patrick Neggie <32044099+patrick9958@users.noreply.github.com>
2018-08-08 16:28:39 -0400

    added a grid, more comments, changed health bonus when dodging

--- plot.py
+++ plot.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import random
 
 
@@ -19,14 +18,14 @@ class Player:
             self.health -= health_penalty
 
     def play_move(self, opponent):
-        rand_num = random.randint(0, len(self.moves) - 1)
-        current_move = self.moves[rand_num]
-        current_strength = self.move_strengths[rand_num]
+        index = random.randint(0, len(self.moves) - 1)
+        current_move = self.moves[index]
+        current_strength = self.move_strengths[index]
 
         bonus = 20
         die_roll = random.randint(1, 20)
         if die_roll > 5:
-            self.move_strengths[rand_num] += bonus      # make winner's move more powerful
+            self.move_strengths[index] += bonus      # make winner's move more powerful
             opponent.lose_battle(current_strength)
             print(self.name + " attacked " + opponent.name + " using " + current_move + "!")
             print(opponent.name + " lost " + str(current_strength) + " health!")
@@ -34,11 +33,11 @@ class Player:
             return self
         else:
             print(opponent.name + " dodged your move!")
-            health_bonus = 15
+            health_bonus = 7
             if opponent.health + health_bonus > 100:
                 opponent.health = 100
             else:
-                opponent.health += health_bonus
+                opponent.health += (health_bonus * 2)
             if self.health + health_bonus > 100:
                 self.health = 100
             else:
@@ -56,8 +55,9 @@ vader = Player("Darth Vader", 100, evil_moves, evil_move_strengths)
 
 move_num = 0
 move_list = [move_num]                          # move_list will be a list: [0, 1, 2, 3, ... , 23, 24, move_num]
-vader_health_list = [vader.health]              # vader_health_list will be a list of his health after every move
-luke_health_list = [luke.health]                # luke_health_list will be a list of his health after every move
+# vader_health_list will be a list of his health after every move i.e. [100, 70, 30, 0]
+vader_health_list = [vader.health]
+luke_health_list = [luke.health]
 while luke.alive and vader.alive:
     move = random.randint(0, 1)
     if move == 0:
@@ -77,10 +77,10 @@ elif vader.alive and not luke.alive:
 plt.xlabel('Moves')
 plt.ylabel('Health')
 plt.title('Health over time')
-
 # create an x-axis that goes from 1 to total_moves
 # create a y-axis that goes from 0 to 100
 plt.axis([1, move_num, 0, 100])
 # Vader is red circles and Luke is green triangles
 plt.plot(move_list, vader_health_list, 'ro', move_list, luke_health_list, 'g^')
+plt.grid(True)
 plt.show()
